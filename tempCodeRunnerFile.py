5417c90fc6c7a1a707627f430a1788a9bfed0852
Niisarga <eznisarga@gmail.com>
2026-03-14 19:42:02 +0530

    Clean up API key comments in tempCodeRunnerFile.py
    Removed commented-out API key and App ID.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,9 +2,6 @@ import requests
 import os
 from datetime import datetime
 
-# API_Key=nix_live_r5agTX6n860PJLgX5zenHuy75qbvBWs7
-# App_ID=app_fbdaf11bcdf3418da3fb5dd9 
-
 APP_ID = os.environ["NT_APP_ID"]
 API_KEY = os.environ["NT_API_KEY"]
 
